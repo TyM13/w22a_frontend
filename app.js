a4362af280318c5ac7ccca985543e791040d4ba1
Your Name <you@example.com>
2022-10-15 08:40:53 -0600

    commented w22a_frontend code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let candy_container = document.getElementById('candy_container');
 
 
 function handle_submit(details) {
+    // sends a api post request that requires the data, candy name, description and candy image then if successful it will create the candy otherwise it will send an error back
     axios.request({
         url: "http://127.0.0.1:5000/api/candy",
         method: "POST",
@@ -16,12 +17,14 @@ function handle_submit(details) {
         submit_button.insertAdjacentHTML('afterend', '<p>Error inserting new candy</p>')
     });
 }
-
+// gets the submit button from the html's id and sets it as the variable submit_button
+// when the submit_button is clicked it will call the function handle submit
 let submit_button = document.getElementById('submit_button');
 submit_button.addEventListener('click', handle_submit)
 
 
-
+// sends a api get request that if successful it will display the candy, put a delete button for the candy and get the delete button and set it as the variable delete_button 
+//when the delete_button is clicked it will call the function handle_delete otherwise it will send an error back to the user (put in so it so the info can be attained in the delete function)
 axios.request({
     url: "http://127.0.0.1:5000/api/candy",
 }).then((response) => {
@@ -40,14 +43,16 @@ axios.request({
         `<h1>error getting candy, please contant us</h1>`)
 })
 
-
+// sends a api get request that if successful it will delete the candy.
 function handle_delete(details) {
     axios.request({
         url: "http://127.0.0.1:5000/api/candy",
         method: "DELETE",
+        // gets the candy_id from details target and gets the attribute candy_id which is passed along from the get request.
         data: {
             candy_id: details['target'].getAttribute('candy_id'),
         }
+        // if it's sucessfully deleted it will display a sucess message otherwise it will send an error that it couldn't delete the candy
     }).then((response) => {
         document.getElementById('candy_container').insertAdjacentHTML('afterend',
             `<h1>sucessfully deleted</h1>`)
